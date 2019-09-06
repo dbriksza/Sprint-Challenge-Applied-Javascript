f7f28dff510a91d0cdac5ad597d0da0ec8fe8173
dbriksza <dbriksza@gmail.com>
2019-09-06 13:53:17 -0400

    Stretch: added infinite carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,59 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const entrypoint = document.querySelector(".carousel-container");
+
+const imgArray = [
+  "./assets/carousel/mountains.jpeg",
+  "./assets/carousel/computer.jpeg",
+  "./assets/carousel/trees.jpeg",
+  "./assets/carousel/turntable.jpeg"
+];
+
+function Carousel(imgs) {
+  let x = 0;
+  const carousel = document.createElement("div");
+  const left = document.createElement("div");
+  const right = document.createElement("div");
+  const img0 = document.createElement("img");
+
+  img0.style.display = "block";
+
+  left.textContent = "<";
+  right.textContent = ">";
+  img0.src = imgs[x];
+
+  carousel.appendChild(left);
+  carousel.appendChild(img0);
+  carousel.appendChild(right);
+
+  carousel.classList.add("carousel");
+  left.classList.add("left-button");
+  right.classList.add("right-button");
+
+  left.addEventListener("click", e => {
+    if (x === 0) {
+      x = 3;
+      img0.src = imgs[x];
+    } else {
+      x--;
+      img0.src = imgs[x];
+    }
+    console.log(x);
+  });
+  right.addEventListener("click", e => {
+    if (x === 3) {
+      x = 0;
+      img0.src = imgs[x];
+    } else {
+      x++;
+      img0.src = imgs[x];
+    }
+    console.log(x);
+  });
+
+  return carousel;
+}
+
+entrypoint.appendChild(Carousel(imgArray));
